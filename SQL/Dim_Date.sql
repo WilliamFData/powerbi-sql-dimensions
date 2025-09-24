ba7fb5745c522b8b73f2214398b14193d0acce4a
William Finglas <willfinglas@hotmail.com>
2025-09-25 09:45:57 +1000

    Update Dim_Date.sql

--- SQL/Dim_Date.sql
+++ SQL/Dim_Date.sql
@@ -1,51 +1,51 @@
--- ====================================================================================================================
+-- ===================================================================
 -- Step 1. Generate Dates
--- ====================================================================================================================
+-- ===================================================================
 CREATE OR REPLACE TABLE DATES AS
 WITH DATE_RANGE AS 
 (
-  SELECT DATEADD(DAY, SEQ4(), DATE '2000-01-01') AS DATE
-  FROM TABLE(GENERATOR(ROWCOUNT => 18628)) -- 1 year of dates is 365
+	SELECT DATEADD(DAY, SEQ4(), DATE '2000-01-01') AS DATE_ID
+	FROM TABLE(GENERATOR(ROWCOUNT => 18628)) -- 1 year of dates is 365
 )
 SELECT * FROM DATE_RANGE
 
 --SELECT * FROM DATES
 
--- ====================================================================================================================
+-- ===================================================================
 -- Step 2. Create logic for date dimension
--- ====================================================================================================================
+-- ===================================================================
 CREATE OR REPLACE TABLE DIM_DATE AS 
 SELECT 
-	  TO_NUMBER(TO_CHAR(DATE, 'YYYYMMDD')) AS DATE_DIM_ID
-	, DATE
-	, DAY(DATE) AS DAY_OF_MONTH
-	, MONTH(DATE) AS MONTH
-	, MONTHNAME(DATE) AS MONTH_NAME_SHORT
-	, TO_CHAR(DATE, 'MMMM') AS MONTH_NAME_FULL
-	, YEAR(DATE) AS YEAR
+	  TO_NUMBER(TO_CHAR(DATE_ID, 'YYYYMMDD')) AS DATE_DIM_ID
+	, DATE_ID
+	, DAY(DATE_ID) AS DAY_OF_MONTH
+	, MONTH(DATE_ID) AS MONTH
+	, MONTHNAME(DATE_ID) AS MONTH_NAME_SHORT
+	, TO_CHAR(DATE_ID, 'MMMM') AS MONTH_NAME_FULL
+	, YEAR(DATE_ID) AS YEAR
 --Weeks (Can be used for Same Day Last Year Calculations)
-	, DAYOFWEEKISO(DATE) AS DAY_OF_WEEK_NUM
-	, TO_CHAR(DATE, 'DY') DAY_OF_WEEK_DESC
-	, WEEKISO(DATE) AS ISO_WEEK
-	, CONCAT(WEEKISO(DATE), '-', DAYOFWEEKISO(DATE)) AS SAME_DAY_KEY
-	, DATE_TRUNC('WEEK', DATE) AS START_OF_WEEK
-	, DATEADD('DAY', 6, DATE_TRUNC('WEEK', DATE)) AS END_OF_WEEK
+	, DAYOFWEEKISO(DATE_ID) AS DAY_OF_WEEK_NUM
+	, TO_CHAR(DATE_ID, 'DY') DAY_OF_WEEK_DESC
+	, WEEKISO(DATE_ID) AS ISO_WEEK
+	, CONCAT(WEEKISO(DATE_ID), '-', DAYOFWEEKISO(DATE_ID)) AS SAME_DAY_KEY
+	, DATE_TRUNC('WEEK', DATE_ID) AS START_OF_WEEK
+	, DATEADD('DAY', 6, DATE_TRUNC('WEEK', DATE_ID)) AS END_OF_WEEK
 --Quarters
-	, QUARTER(DATE) AS QUARTER
+	, QUARTER(DATE_ID) AS QUARTER
 --Years
 	, YEAR + CASE WHEN MONTH >= 7 THEN 1 ELSE 0 END AS FIN_YEAR
-	, YEAROFWEEKISO(DATE) AS ISO_YEAR
+	, YEAROFWEEKISO(DATE_ID) AS ISO_YEAR
 --Display 
-	, TO_CHAR(DATE, 'MON-YYYY') AS MONTH_YEAR
+	, TO_CHAR(DATE_ID, 'MON-YYYY') AS MONTH_YEAR
 	, CONCAT('FY',FIN_YEAR) AS FIN_YEAR_DESC
 --Offsets
-	, DATEDIFF(DAY, DATE, GETDATE()::DATE) * -1 AS DAY_OFFSET
-	, DATEDIFF(WEEK, DATE, GETDATE()::DATE) * -1 AS WEEK_OFFSET
-	, DATEDIFF(MONTH, DATE, GETDATE()::DATE) * -1 AS MONTH_OFFSET
-	, DATEDIFF(YEAR, DATE, GETDATE()::DATE) * -1 AS YEAR_OFFSET
+	, DATEDIFF(DAY, DATE_ID, GETDATE()::DATE) * -1 AS DAY_OFFSET
+	, DATEDIFF(WEEK, DATE_ID, GETDATE()::DATE) * -1 AS WEEK_OFFSET
+	, DATEDIFF(MONTH, DATE_ID, GETDATE()::DATE) * -1 AS MONTH_OFFSET
+	, DATEDIFF(YEAR, DATE_ID, GETDATE()::DATE) * -1 AS YEAR_OFFSET
 FROM DATES
 WHERE 1=1
-	AND DATE <= DATEADD(DAY,-1,GETDATE()::DATE)--For if you only want historical dates
+	AND DATE_ID <= DATEADD(DAY,-1,GETDATE()::DATE)--For if you only want historical dates
 ORDER BY 1 DESC 
 
 --SELECT * FROM DIM_DATE
